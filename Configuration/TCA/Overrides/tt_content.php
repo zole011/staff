e74b7109118fa6e2e3bd6f89f48e6d17c053fb55
zole011 <zoran.it@gmail.com>
2025-05-11 23:28:59 +0200

    commit 1234567890abcdef1234567890abcdef12345678

--- Configuration/TCA/Overrides/tt_content.php
+++ Configuration/TCA/Overrides/tt_content.php
@@ -1,4 +1,5 @@
 <?php
+
 \TYPO3\CMS\Core\Utility\ExtensionManagementUtility::addPlugin(
     ['Team Members', 'staff_team'],
     'CType',
--- ext_tables.php
+++ ext_tables.php
@@ -4,12 +4,3 @@
     'Configuration/TypoScript',
     'Staff'
 );
-
-\TYPO3\CMS\Core\Utility\ExtensionManagementUtility::addPlugin(
-    [
-        'Staff Plugin',
-        'staff',
-        'EXT:staff/Resources/Public/Icons/user_plugin_staff.svg'
-    ],
-    'list_type'
-);
