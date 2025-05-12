2eb6aae9ab448ee23ca394434315f2a62e1f5924
zole011 <zoran.it@gmail.com>
2025-05-12 11:23:27 +0200

    commit neewed

---
+++ Configuration/TCA/Overrides/sys_template.php
@@ -0,0 +1,4 @@
+<?php
+defined('TYPO3') or die();
+
+\TYPO3\CMS\Core\Utility\ExtensionManagementUtility::addStaticFile('staff', 'Configuration/TypoScript', 'Staff Management');
--- ext_localconf.php
+++ ext_localconf.php
@@ -1,17 +1,14 @@
 <?php
+defined('TYPO3') or die();
 
-defined('TYPO3') || die();
-
-call_user_func(function () {
-    \TYPO3\CMS\Extbase\Utility\ExtensionUtility::configurePlugin(
-        'Gmbit.Staff',
-        'Team',
-        [
-            \Gmbit\Staff\Controller\TeamController::class => 'list, show'
-        ],
-        // Non-cacheable actions
-        [
-            \Gmbit\Staff\Controller\TeamController::class => 'list, show'
-        ]
-    );
-});
+\TYPO3\CMS\Core\Utility\ExtensionManagementUtility::addModule(
+    'web', // Parent module
+    'teamadmin', // Module key
+    '', // Position (leave empty to use default)
+    \TYPO3\CMS\Core\Utility\ExtensionManagementUtility::extPath('staff') . 'Resources/Private/Icons/user_mod_team.svg', // Icon
+    [
+        'access' => 'user,group', // Permissions
+        'icon' => 'EXT:staff/Resources/Public/Icons/user_mod_team.svg', // Icon
+        'labels' => 'LLL:EXT:staff/Resources/Private/Language/locallang_mod_team.xlf', // Labels
+    ]
+);
